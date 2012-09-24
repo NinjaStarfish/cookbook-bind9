eb9429fe903c9647611bbc010ee1cde93b4c05a0
Craig Gardener <craig@ninjastarfish.co.uk>
2012-09-24 07:04:06 +0100

    Created BIND9 cookbook

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Ninja Starfish"
+maintainer_email "chef@ninjastarfish.co.uk"
+license          "Apache 2.0"
+description      "Installs/Configures bind9"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
